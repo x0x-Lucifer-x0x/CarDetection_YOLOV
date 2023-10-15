0de5daff0923476bbf887d42af61e10e7d9b7564
x0x-Lucfier-x0x <meetvachhani77@gmail.com>
2023-10-15 22:01:13 +0530

    changes

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-import os
+from ultralytics import YOLO
+
+model = YOLO("yolov8n.yaml") # build new model from scratch
+
+results = model.train(data='config.yaml', epochs='1') #train the model
+
+
+
 
 
 
